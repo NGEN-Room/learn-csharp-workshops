7b9a85bbac2166e174762fc8f0343d467bc8188b
CoolmateJJ <coolmatejj23@gmail.com>
2025-04-22 16:00:29 +1200

    Completed Modual 4

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Enter first number");
+double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Choose Operator: +, -, *, /");
+string Operator = Console.ReadLine();
+
+double sum = 0;
+
+ if (Operator == "+")
+{
+    sum = firstNumber + secondNumber;
+} else if (Operator == "-")
+{
+    sum = firstNumber - secondNumber;
+} else if (Operator == "*")
+{
+    sum = firstNumber * secondNumber;
+} else if (Operator == "/")
+{
+    sum = firstNumber / secondNumber;
+}
+
+Console.WriteLine(sum);
