af11bc4d686aff1ecdf0ea6828f94a328d50c4b8
CoolmateJJ <coolmatejj23@gmail.com>
2025-04-29 16:05:48 +1200

    updated alot

---
+++ AccessChecker/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter Age:");
+int age = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (age >= 18)
+{
+    Console.WriteLine("Access Granted, Welcome to the Just B Club");
+
+}
+
+else 
+{
+    Console.WriteLine("Do you have a valid ticket?");
+    string Boolean = Console.ReadLine();
+    if (Boolean.Contains("yes", StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine("Access Granted, Welcome to the Just B Club");
+    }
+    else if (Boolean.Contains("no", StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine("Access Denied, Sorry but your under the age, come back when your older");
+    }
+    
+
+}
---
+++ UsernameGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter First name:");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Enter Last name:");
+string lastName = Console.ReadLine();
+
+Random rng = new Random();
+int RanNum = rng.Next(100);
+
+Console.WriteLine(firstName.Substring(0,1) + lastName.ToLower() + RanNum);
+
